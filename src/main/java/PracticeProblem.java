4a61fba3dbc6d4b4de76372e496418d3b7abd97d
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-04-03 18:47:02 +0000

    Problems Complete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,48 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class PracticeProblem {
 
-	public static void main(String args[]) {
+public class PracticeProblem{
+	static ArrayList<String> arr;
+	static String string;
 
+	static void permutate(String cur, int visited, boolean unique){
+		if(cur.length() == string.length()){
+			arr.add(cur);
+			return;
+		}
+		boolean[] letters = new boolean[256];
+		for(int i = 0; i < string.length(); ++i){
+			int ch = string.charAt(i);
+			if((visited & 1 << i) == 0){
+				if((unique && !letters[ch]) || !unique){
+					permutate(cur+string.charAt(i), visited ^ 1 << i, unique);
+				}else{
+					continue;
+				}
+				if(unique){
+					letters[ch] = true;
+				}
+			}
+		}
 	}
 
+	static ArrayList<String> perms(String str){
+		char[] charArr = str.toCharArray();
+		Arrays.sort(charArr);
+		string = new String(charArr);
+		arr = new ArrayList<String>();
+		permutate("", 0, false);
+		return arr;
+	}
+
+	static ArrayList<String> permsUnique(String str){
+		char[] charArr = str.toCharArray();
+		Arrays.sort(charArr);
+		string = new String(charArr);
+		arr = new ArrayList<String>();
+		permutate("", 0, true);
+		return arr;
+	}
+	public static void main(String args[]){}
 }
